6d59e500a2a344ed2e44b3ddbb90193bd35116f0
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2022-10-28 16:52:51 +0900

    CPUの原型

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+pub struct CPU {
+    pub register_a: u8,
+    pub status: u8,
+    pub program_counter: u16,
+}
+
+impl CPU {
+    pub fn new() -> Self {
+        CPU {
+            register_a: 0,
+            status: 0,
+            program_counter: 0,
+        }
+    }
+
+    pub fn interpret(&mut self, program: Vec<u8>) {
+        self.program_counter = 0;
+        loop {
+            let opcode = program[self.program_counter as usize];
+            self.program_counter += 1;
+
+            match opcode {
+                _ => todo!(),
+            }
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
