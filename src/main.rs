bad71b620d55e8381c10bf91bf8e6282f8b02dca
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2022-10-28 17:47:34 +0900

    0xaaの実装

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 pub struct CPU {
     pub register_a: u8,
+    pub register_x: u8,
     pub status: u8,
     pub program_counter: u16,
 }
@@ -8,6 +9,7 @@ impl CPU {
     pub fn new() -> Self {
         CPU {
             register_a: 0,
+            register_x: 0,
             status: 0,
             program_counter: 0,
         }
@@ -20,6 +22,7 @@ impl CPU {
             self.program_counter += 1;
 
             match opcode {
+                // LDA
                 0xA9 => {
                     let param = program[self.program_counter as usize];
                     self.program_counter += 1;
@@ -32,6 +35,22 @@ impl CPU {
                     }
 
                     if self.register_a & 0b1000_0000 != 0 {
+                        self.status = self.status | 0b1000_0000;
+                    } else {
+                        self.status = self.status & 0b0111_1111;
+                    }
+                }
+                // TAX
+                0xAA => {
+                    self.register_x = self.register_a;
+
+                    if self.register_x == 0 {
+                        self.status = self.status | 0b0000_0010;
+                    } else {
+                        self.status = self.status & 0b1111_1101;
+                    }
+
+                    if self.register_x & 0b1000_0000 != 0 {
                         self.status = self.status | 0b1000_0000;
                     } else {
                         self.status = self.status & 0b0111_1111;
@@ -76,6 +95,16 @@ mod test {
         let mut cpu = CPU::new();
         cpu.interpret(vec![0xa9, 0x80, 0x00]);
         assert_eq!(cpu.register_a, 0x80);
+        assert!(cpu.status & 0b1000_0000 == 0b1000_0000);
+    }
+
+    #[test]
+    fn test_0xaa_tax_move_a_to_x() {
+        let mut cpu = CPU::new();
+        cpu.register_a = 0x83;
+        cpu.interpret(vec![0xaa, 0x00]);
+        assert_eq!(cpu.register_x, 0x83);
+        assert!(cpu.status & 0b0000_0010 == 0b00);
         assert!(cpu.status & 0b1000_0000 == 0b1000_0000);
     }
 }
