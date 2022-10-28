60a8b45506c6257b0f83374b6d3e36999963453e
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2022-10-28 17:33:19 +0900

    最初のopcode実装

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,26 @@ impl CPU {
             self.program_counter += 1;
 
             match opcode {
+                0xA9 => {
+                    let param = program[self.program_counter as usize];
+                    self.program_counter += 1;
+                    self.register_a = param;
+
+                    if self.register_a == 0 {
+                        self.status = self.status | 0b0000_0010;
+                    } else {
+                        self.status = self.status & 0b1111_1101;
+                    }
+
+                    if self.register_a & 0b1000_0000 != 0 {
+                        self.status = self.status | 0b1000_0000;
+                    } else {
+                        self.status = self.status & 0b0111_1111;
+                    }
+                }
+                0x00 => {
+                    return;
+                }
                 _ => todo!(),
             }
         }
@@ -28,4 +48,34 @@ impl CPU {
 
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_0xa9_lda_immediate_load_data() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa9, 0x05, 0x00]);
+        assert_eq!(cpu.register_a, 0x05);
+        assert!(cpu.status & 0b0000_0010 == 0b00);
+        assert!(cpu.status & 0b1000_0000 == 0);
+    }
+
+    #[test]
+    fn test_0xa9_lda_zero_flag() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa9, 0x00, 0x00]);
+        assert_eq!(cpu.register_a, 0x00);
+        assert!(cpu.status & 0b0000_0010 == 0b10);
+    }
+
+    #[test]
+    fn test_0xa9_lda_negative_flag() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa9, 0x80, 0x00]);
+        assert_eq!(cpu.register_a, 0x80);
+        assert!(cpu.status & 0b1000_0000 == 0b1000_0000);
+    }
 }
