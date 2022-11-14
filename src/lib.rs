693c8138439feaa4edb2356c9777a484c07dfc67
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2022-11-14 17:01:16 +0900

    PPUのテストを通す

--- src/lib.rs
+++ src/lib.rs
@@ -9,4 +9,6 @@ pub mod ppu_control_register;
 pub mod ppu_mask_register;
 pub mod ppu_scroll_register;
 pub mod ppu_status_register;
+pub mod renderer_frame;
+pub mod renderer_palette;
 pub mod trace;
